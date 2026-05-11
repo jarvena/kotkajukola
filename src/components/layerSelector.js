60fcfdcbb24479f7752f6783e1d2439d5a686be6
jarvena <antti.jarvenpaa@terrasolid.com>
2026-05-11 08:45:45 +0300

    Lisää metsäilmoitukset

--- src/components/layerSelector.js
+++ src/components/layerSelector.js
@@ -151,7 +151,7 @@ export default function LayerSelector({bgState, overlayVisibilityState}) {
             <FormControlLabel control={<Checkbox defaultChecked onChange={(e) => setOverlayVisibility(e, overlayVisibilityState, ['oldMap'])} sx={{ color: 'white' }}/>} label={t('oldMap')} />
             {/* <FormControlLabel control={<Checkbox defaultChecked onChange={(e) => setOverlayVisibility(e, overlayVisibilityState, ['arena'])} sx={{ color: 'white' }}/>} label={t('eventCentre')} /> */}
             <FormControlLabel control={<Checkbox defaultChecked onChange={(e) => setOverlayVisibility(e, overlayVisibilityState, ['tentFill', 'tentBorder', 'tentLabel'])} sx={{ color: 'white' }}/>} label={t('Tents')} />
-            {/* <FormControlLabel control={<Checkbox defaultChecked onChange={(e) => setOverlayVisibility(e, overlayVisibilityState, ['forestFill'])} sx={{ color: 'white' }}/>} label={t('Harvests')} /> */}
+            <FormControlLabel control={<Checkbox defaultChecked onChange={(e) => setOverlayVisibility(e, overlayVisibilityState, ['forestFill'])} sx={{ color: 'white' }}/>} label={t('Harvests')} />
           </FormGroup>
         </Box>
       </SwipeableDrawer>
